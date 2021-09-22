c859abcab08d9b13b04d3bfa48c194bfbbd52b08
lukas502 <lukas.vorndamme@gmail.com>
2021-09-22 09:26:35 +0200

    hallowelt Zweitesprogramm

---
+++ Scracht to java/src/Hallowelt.java
@@ -0,0 +1,6 @@
+public class Hallowelt {
+    public static void main(String[] args) {
+        System.out.println("Hallowelt");
+    }
+
+}
---
+++ Scracht to java/src/Zweitesprogramm.java
@@ -0,0 +1,18 @@
+public class Zweitesprogramm {
+    public static void main(String[] args) {
+
+
+
+    int zahl1 = 5;
+    int zahl2 = 4;
+    int ergebnis;
+    ergebnis = zahl1 + zahl2;
+    System.out.println(ergebnis);} }
+
+
+
+
+
+
+
+
