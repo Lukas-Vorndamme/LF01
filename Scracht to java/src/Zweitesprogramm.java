c1e35bee47fa8ec7b297ab4b5dd160af2fb92513
lukas502 <lukas.vorndamme@gmail.com>
2021-09-22 10:58:52 +0200

    hallowelt Zweitesprogramm

--- Scracht to java/src/Zweitesprogramm.java
+++ Scracht to java/src/Zweitesprogramm.java
@@ -1,17 +1,11 @@
 public class Zweitesprogramm {
     public static void main(String[] args) {
 
-
-
     int zahl1 = 5;
     int zahl2 = 4;
     int ergebnis;
     ergebnis = zahl1 + zahl2;
-    System.out.println(ergebnis);} }
-
-
-
-
+    System.out.println("Das Ergebnis lautet "+ergebnis);} }
 
 
 
