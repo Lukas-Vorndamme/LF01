8e06db9c39ea81856797bcd7013e43752d9e0589
lukas502 <lukas.vorndamme@gmail.com>
2021-09-23 12:20:42 +0200

    hallowelt Zweitesprogramm Dreieck

---
+++ Scracht to java/src/Dreieck.java
@@ -0,0 +1,10 @@
+public class Dreieck {
+    public static void main(String[] args){
+
+        int höhe = 5;
+        int grundlinie = 6;
+
+        int ergebnis;
+
+        ergebnis = grundlinie * höhe / 2;
+        System.out.println("Das Ergebnis lautet "+ergebnis);} }
