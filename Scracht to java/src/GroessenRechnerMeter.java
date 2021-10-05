86561c84883922f8e7f1ac844b162daf70196624
lukas502 <lukas.vorndamme@gmail.com>
2021-10-05 10:14:36 +0200

    GroessenRechnerMeter GrossenrRechnerZentimeter

--- Scracht to java/src/GroessenRechnerMeter.java
+++ Scracht to java/src/GroessenRechnerMeter.java
@@ -2,21 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class GroessenRechner {
+
+public class GroessenRechnerMeter {
     public static void main(String[] args) throws IOException {
-        System.out.println("Gibt die leange in cm an");
+        System.out.println("Gib die länge in meter an");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String eingabe= br.readLine();
 
-        double cm = Double.parseDouble(eingabe.trim().replace(',', '.'));
-        double mm = cm*10;
-        double dm = cm/10;
-        double m = cm/100;
-        double km = cm/ 100000;
+        double m = Double.parseDouble(eingabe.trim().replace(',','.'));
+        double mm = m/1000;
+        double cm = m/100;
+        double dm = m/10;
+        double km = m*1000;
 
         System.out.println("Die leange ist in mm \t" + mm );
+        System.out.println("Die leange ist in cm \t" + cm );
         System.out.println("Die leange ist in dm \t" + dm );
-        System.out.println("Die leange ist in m \t" + m );
         System.out.println("Die leange ist in km \t" + km );
 
     }
