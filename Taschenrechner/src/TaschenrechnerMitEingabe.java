24f25841350faf31218f31299efac1ccfcb41ba0
lukas502 <lukas.vorndamme@gmail.com>
2021-10-11 08:30:38 +0200

    hallowelt Zweitesprogramm Dreieck Regen trapez NotenRechner Dreiecksungleichung Kilometer Meilen TaschenrechnerMitEingabe TaschenrechnerOhneEingabe TaschenrechnerMitZahlEingabe

--- Taschenrechner/src/TaschenrechnerMitEingabe.java
+++ Taschenrechner/src/TaschenrechnerMitEingabe.java
@@ -4,29 +4,29 @@ import java.io.InputStreamReader;
 
 public class TaschenrechnerMitEingabe {
     public static void main(String[] args) throws IOException {
-        System.out.println("Gib die erste zahl an ");
+        System.out.println("Gib die Erste Zahl an ");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String eingabe = br.readLine();
 
         double  zahl1 =  Double.parseDouble(eingabe.trim().replace(',', '.'));
-        System.out.println(" Gib die zweite zahl ein ");
+        System.out.println(" Gib die Zweite Zahl ein ");
         eingabe = br.readLine();
         double zahl2 = Double.parseDouble(eingabe.trim().replace(',', '.'));
 
-        System.out.println("Gib die rechenoperation an ");
+        System.out.println("Gib die Rechenoperation an ");
 
         String rechenoperator = br.readLine();
         if (rechenoperator.equals("+")){
-            System.out.println("Das ergebnis ist " + (zahl1+zahl2));
+            System.out.println("Das Ergebnis ist " + (zahl1+zahl2));
         }
         else if (rechenoperator.equals("-")) {
-            System.out.println( " Das ergebnis ist " + (zahl1-zahl2));
+            System.out.println( " Das Ergebnis ist " + (zahl1-zahl2));
         }
         else if (rechenoperator.equals("*")) {
-            System.out.println("Das ergebnis ist " + zahl1 * zahl2);
+            System.out.println("Das Ergebnis ist " + (zahl1 * zahl2));
         }
         else if (rechenoperator.equals("/")) {
-            System.out.println("Das ergebnis ist " + zahl1 / zahl2);
+            System.out.println("Das Ergebnis ist " + (zahl1 / zahl2));
         }
 
 
