440b3b5cc57199b7df4101a8f9cd313405ab1e4c
lukas502 <lukas.vorndamme@gmail.com>
2021-11-18 13:44:55 +0100

    hallowelt Zweitesprogramm Dreieck Regen trapez NotenRechner Dreiecksungleichung Kilometer Meilen TaschenrechnerMitEingabe TaschenrechnerOhneEingabe TaschenrechnerMitZahlEingabe TaschenrechnerFinale TaschenrachnerFinaleVersuch2 BloeckeTaschenrechner Mathematik IO IOtest

---
+++ Arbeit/src/Zellwachstum.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+public class Zellwachstum {
+    public static void main(String[] args) throws IOException {
+
+        boolean wiederholen;
+        do {
+            System.out.println("Gib die dei aktuelle zallmenge an");
+            double zellmengen = IO.readDouble();
+            System.out.println("Gib den wachstumsfaktor am");
+            double wachstumsfaktor = IO.readDouble();
+            System.out.println("Wie viele tage sollen berechnet werden");
+            int tage = IO.readInteger();
+            double zellmenge = zellmengen;
+            System.out.println("Sartzellen    \t \t \t" + zellmengen);
+            System.out.println("Wachstumsfaktor\t\t\t " + wachstumsfaktor);
+            System.out.println("Tage der wiederholung\t " + tage);
+            System.out.println("Tag 0\t\t " + zellmengen);
+            for (int n = 1; n <= tage; n++) {
+                zellmenge = zellmenge * wachstumsfaktor;
+                System.out.println("Tag " + n + "\t" + "\t " + zellmenge);
+            }
+            System.out.println("Möchten sie Weiter Wachstumsprozesse berechnen?");
+            wiederholen = IO.readBoolean();
+        } while (wiederholen);
+
+    }
+}
