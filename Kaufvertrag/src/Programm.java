dec597b0023a4e08cd5abe81c5231324c08a2c08
lukas502 <lukas.vorndamme@gmail.com>
2022-05-18 12:16:12 +0200

    Arrays

--- Kaufvertrag/src/Programm.java
+++ Kaufvertrag/src/Programm.java
@@ -1,5 +1,7 @@
 import businessObjects.Vertragspartner;
+import businessObjects.Ware;
 import dao.Vertragspartnerdao;
+import dao.Waredao;
 
 public class Programm {
     public static void main(String[] args) throws ClassNotFoundException {
@@ -7,5 +9,9 @@ public class Programm {
         Vertragspartnerdao vertragspartnerdao = new Vertragspartnerdao();
         Vertragspartner vertragspartner = vertragspartnerdao.read("396435436439");
         System.out.println(vertragspartner.getVorname());
+
+        Waredao waredao = new Waredao();
+        Ware ware = waredao.read("1");
+        System.out.println(ware.getPreis()+"€");
     }
 }
--- Kaufvertrag/src/dao/Waredao.java
+++ Kaufvertrag/src/dao/Waredao.java
@@ -1,15 +1,55 @@
 package dao;
 
-import java.security.PublicKey;
+import businessObjects.Ware;
+
+import java.sql.*;
 
 public class Waredao {
     private final String CLASSNAME ="org.sqlite.JDBC";
-    private final String CONNECTIONSTRING = "jdbc:sqlite:Waredao/src/data/Kaufvertrag.db";
-
+    private final String CONNECTIONSTRING = "jdbc:sqlite:Kaufvertrag/src/data/Kaufvertrag.db";
 
     public Waredao () throws ClassNotFoundException {
         Class.forName(CLASSNAME);
     }
+    public Ware read(String warenNr){
+        Ware ware = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = DriverManager.getConnection(CONNECTIONSTRING);
+
+            //SQL-Abfrage erstellen
+            String sql = "SELECT * From ware WHERE warenNr = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,warenNr);
+
+            //SQL-Abfrage ausführen
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //Zeiger auf den ersten Datensatz setzen
+            resultSet.next();
 
 
+            String nr = resultSet.getString("warenNr");
+            String bezeichnung = resultSet.getString("bezeichnung");
+            String beschreibung = resultSet.getString("beschreibung");
+            double preis = resultSet.getDouble("preis");
+
+
+            ware = new Ware(bezeichnung,preis);
+            ware.setBeschreibung(beschreibung);
+            ware.setBezeichnung(bezeichnung);
+            ware.setPreis(preis);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+         return ware;
+        }
 }
